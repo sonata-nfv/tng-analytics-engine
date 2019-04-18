b3dfeda115f91aac4900edca63798236f48c2dda
efotopoulou <fwtopoulou@gmail.com>
2019-04-18 17:33:27 +0300

    enable cross origin reference

--- src/main/java/eu/tng/graphprofiler/GPApplication.java
+++ src/main/java/eu/tng/graphprofiler/GPApplication.java
@@ -2,12 +2,15 @@ package eu.tng.graphprofiler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAsync
 @EnableMongoRepositories("eu.tng.repository.dao")
+@ComponentScan({"eu.tng"})
 public class GPApplication {
 
     public static void main(String[] args) {
--- src/main/java/eu/tng/graphprofiler/GPController.java
+++ src/main/java/eu/tng/graphprofiler/GPController.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import eu.tng.api.exception.CustomNotFoundException;
 import eu.tng.repository.dao.AnalyticResultRepository;
 import eu.tng.repository.dao.AnalyticServiceRepository;
 import eu.tng.repository.domain.AnalyticResult;
@@ -34,17 +35,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -96,6 +101,10 @@ public class GPController {
     //Fetch all available analytic services
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String getAnalyticServiceList() {
+
+//        if (true) {
+//            throw new CustomNotFoundException("Not found customer with name is ");
+//        }
         analyticServiceRepository.deleteAll();
 
         // save a couple of customers
