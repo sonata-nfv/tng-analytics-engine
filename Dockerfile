ad9da816c0e50a88811de0f0d4adf2fa24e353c2
efotopoulou <fwtopoulou@gmail.com>
2019-06-07 18:25:22 +0300

    update execution of analytics service. containirize analytics engine

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM openjdk
 WORKDIR /profiler
 RUN cd /profiler
 EXPOSE 8082
-COPY /target/tng-profiler-0.0.1-SNAPSHOT.jar  .
+COPY /target/tng-analytics-engine-0.0.1-SNAPSHOT.jar  .
 
 #ENV MONGO_DB mongo
 
-CMD ["java","-jar","tng-profiler-0.0.1-SNAPSHOT.jar"]
+CMD ["java","-jar","tng-analytics-engine-0.0.1-SNAPSHOT.jar"]
--- src/main/java/eu/tng/graphprofiler/GPController.java
+++ src/main/java/eu/tng/graphprofiler/GPController.java
@@ -85,43 +85,70 @@ public class GPController {
 
         // save a couple of customers
         AnalyticService as1 = new AnalyticService();
-        as1.setName("ChordDiagram");
-        as1.setUrl("/ocpu/library/Physiognomica/R/getChordDiagramFromPrometheusMetrics");
-        as1.setDescription("Provide a correlogram in the form of an interactive Chord diagram");
+        as1.setName("correlogram");
+        as1.setUrl("/ocpu/library/Physiognomica/R/correlogram");
+        as1.setDescription("Provide a correlogram with high statistical correlations between metrics");
         as1.setConstraints("Select the set of metrics (more than one) to be used for the calculation of the correlation matrix");
         
-        List<String> results1 = new LinkedList<String>();
-        results1.add("correlation_page.html");
-        results1.add("correlation_matrix.csv");
-        results1.add("correlation_matrix.json");
-        results1.add("metrics_appendix.csv");
+        List<String> results1 = new LinkedList<>();
+        results1.add("correlogram.html");
         as1.setResults(results1);
         analyticServiceRepository.save(as1);
         
+        
+        AnalyticService as5 = new AnalyticService();
+        as5.setName("chord");
+        as5.setUrl("/ocpu/library/Physiognomica/R/chord");
+        as5.setDescription("Provide a correlogram with high statistical correlations between metrics via an interactive chord diagram");
+        as5.setConstraints("Select the set of metrics (more than one) to be used for the calculation of the correlation matrix");
+        List<String> results5 = new LinkedList<>();
+        results5.add("correlation_page.html");
+        as5.setResults(results5);
+        analyticServiceRepository.save(as5);
+        
         AnalyticService as2 = new AnalyticService();
         as2.setName("TimeSeriesDecomposition");
-        as2.setUrl("/ocpu/library/Physiognomica/R/timeSeriesDecomposition");
+        as2.setUrl("/ocpu/library/Physiognomica/R/time_series_decomposition");
         as2.setDescription("Provide a decomposition of a time series in seasonal, trend, and remainder parts");
         as2.setConstraints("Select one metric to be used for time series decomposition");
-        
-        List<String> results2 = new LinkedList<String>();
+        List<String> results2 = new LinkedList<>();
         results2.add("time_series_decomposition.html");
-        results2.add("tm_series_decomposition.jpg");
-        as2.setResults(results2);
-        analyticServiceRepository.save(as1);
-        
+        as2.setResults(results2);      
         analyticServiceRepository.save(as2);
         
         AnalyticService as3 = new AnalyticService();
-        as3.setName("LinearRegression");
-        as3.setUrl("/ocpu/library/Physiognomica/R/combinePrometheusMetrics");
+        as3.setName("linear_regression");
+        as3.setUrl("/ocpu/library/Physiognomica/R/linear_regression");
         as3.setDescription("Provide a linear regression model along with a scatterplot");
         as3.setConstraints("Select the dependent and intenpendent variable for the linear regression model");
-        List<String> results3 = new LinkedList<String>();
-        results3.add("finaldata.csv");
-        results3.add("metricsCombination.html");
+        List<String> results3 = new LinkedList<>();
+        results3.add("linear_regression.html");
         as3.setResults(results3);
         analyticServiceRepository.save(as3);
+       
+        
+        AnalyticService as4 = new AnalyticService();
+        as4.setName("multiple_linear_regression");
+        as4.setUrl("/ocpu/library/Physiognomica/R/multiple_linear_regression");
+        as4.setDescription("Provide a multiple linear regression model along with composed visualizations");
+        as4.setConstraints("Select the dependent and intenpendent variable for the linear regression model");
+        List<String> results4 = new LinkedList<>();
+        results4.add("multiple_linear_regression.html");
+        as4.setResults(results4);
+        analyticServiceRepository.save(as4);
+        
+        
+        
+        AnalyticService as6 = new AnalyticService();
+        as6.setName("test");
+        as6.setUrl("/ocpu/library/Physiognomica/R/test");
+        as6.setDescription("test");
+        as6.setConstraints("test");
+        List<String> results6 = new LinkedList<>();
+        results6.add("console");
+        as6.setResults(results6);
+        analyticServiceRepository.save(as6);
+        
         
         List<AnalyticService> analyticServicesList = analyticServiceRepository.findAll();
         JSONArray asl = new JSONArray(analyticServicesList);
@@ -197,10 +224,10 @@ public class GPController {
     }
 
     //Get all prometheus metrics for a specific nsr_id
-    @RequestMapping(value = "/services/{nsr_id}/metrics", method = RequestMethod.GET)
+    @RequestMapping(value = "/services/vnv/{nsr_id}/metrics", method = RequestMethod.GET)
     public String get5gtangoNetworkServiceMetrics(@PathVariable("nsr_id") String nsr_id) {
         
-        List<String> filteredData = graphProfilerService.get5gtangoNetworkServiceMetrics(nsr_id);
+        List<String> filteredData = graphProfilerService.get5gtangoVnVNetworkServiceMetrics(nsr_id);
         
         JSONObject result = new JSONObject();
         result.put("status", "success");
